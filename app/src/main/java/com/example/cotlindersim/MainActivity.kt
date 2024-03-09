c9e97d67cfe8aee58294aec39e1851bc591cdf86
ferdi-kanat <200101038@ogrenci.yalova.edu.tr>
2024-03-09 16:20:26 +0300

    first commit

---
+++ app/src/main/java/com/example/cotlindersim/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cotlindersim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
